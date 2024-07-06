c12a42029e1036933794b732222033d7e83028ef
Kevin Bonds <kevin.bonds@gmail.com>
2024-07-06 11:40:18 -0500

    Simple Background color changer with JavaScript

---
+++ script.js
@@ -0,0 +1,19 @@
+function setBgGreen() {
+  document.body.style.backgroundColor = "green";
+}
+
+function setBgRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+function setBgBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+function setBgYellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+
+function defaultBgColor() {
+  document.body.style.backgroundColor = "white";
+}
